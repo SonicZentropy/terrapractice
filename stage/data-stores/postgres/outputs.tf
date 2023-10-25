7da717da00b7fc92a42d50c16a4062ae755cc56d
Casey Bailey <caseybailey@gmail.com>
2023-10-25 13:56:49 -0500

    Remove old crud

--- stage/data-stores/postgres/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "address" {
-  value = module.postgres.address
-  description = "Connect to the database at this endpoint"
-}
-
-output "port" {
-  value = module.postgres.port
-  description = "The port the database is listening on"
-}
--- stage/data-stores/postgres/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-db_username = "doggr_db_user"
-db_password = "doggrP4SSw0rd"
